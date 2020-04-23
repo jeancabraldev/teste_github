fadf0ae38b0d9e467fc6c798c547d33bcd2e889b
jeancabraldev <jeancabraldevelopment@gmail.com>
2020-04-23 20:15:50 -0300

    segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Teste Github'),
+      home: MyHomePage(title: 'Teste Github 2'),
     );
   }
 }
